8761fb8ffbb63c4173a7749150f7819e08f93f97
abdulrafaykaispe <your_email@example.com>
2025-09-23 19:58:44 +0500

    before implementing the form

--- src/services/apiForm.js
+++ src/services/apiForm.js
@@ -11,6 +11,21 @@ export async function getForms() {
   return data;
 }
 
+export async function getFormById(id) {
+  const { data, error } = await supabase
+    .from("forms")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error(`Form with ID ${id} could not be loaded`);
+  }
+
+  return data;
+}
+
 export async function createForm(templateName) {
   const { data, error } = await supabase
     .from("forms")
